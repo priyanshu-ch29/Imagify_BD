1bb2752f3e2025b7fee262477a8ec3a65177f292
Priyanshu <91934414+priyanshu-ch29@users.noreply.github.com>
2025-12-16 11:20:08 +0530

    change env

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://priyanshuchaudhary1000:w3fxI1SlMnoxZuJ3@cluster0.cjvgh.mongodb.net/imagifyDB`);
+        await mongoose.connect(`${process.env.MONGO_URL}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Database connected successfully");
     } catch (error) {
         throw new Error(error.message)
